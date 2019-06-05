31ea179606ca37835765e2427b1bb7754081f91d
Marco Pavan <marcopavan.mp@gmail.com>
2019-06-05 21:10:20 +0200

    Add fonts with webfontloader

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import WebFont from 'webfontloader';
 import * as serviceWorker from './serviceWorker';
+
+WebFont.load({
+    google: {
+        families: ['Montserrat:400,700', 'Open+Sans', 'Material+Icons', 'sans-serif']
+    }
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
